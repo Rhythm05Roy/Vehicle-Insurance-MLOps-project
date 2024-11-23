e7ff7c1bc702f4543cd818397d1ed170152d4288
Rhythm05Roy <rhythmroy03@gmail.com>
2024-11-23 23:38:31 +0600

    Project structure is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'src',
+    version = '0.0.1',
+    author = 'Rhythm Roy',
+    author_email = 'rhythmroy03@gmail.com',
+    packages = find_packages() 
+)
