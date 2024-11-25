41736abdc73edf8cf197a36a5d5f4f3a04346a24
Rhythm05Roy <rhythmroy03@gmail.com>
2024-11-25 13:12:25 +0600

    Data ingestion is successfull

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-## Check logger
+# Check logger
 # from src.logger import logging
 # logging.debug("This is a debug message")
 # logging.info("This is an info message")
@@ -8,11 +8,17 @@
 
 ## Check exception
 
-from src.logger import logging
-from src.exception import myException
-import sys
-try:
-    a = 1/0
-except Exception as e:
-    logging.info(e)
-    raise myException(e, sys) from e
+# from src.logger import logging
+# from src.exception import myException
+# import sys
+# try:
+#     a = 1/0
+# except Exception as e:
+#     logging.info(e)
+#     raise myException(e, sys) from e
+
+# mongodb+srv://vikashdas770:WtwsW3eh6T3J0h6z@cluster0.0aygk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+from src.pipline.training_pipeline import TrainPipeline
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
